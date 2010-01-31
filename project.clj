2460f89a09cea437a0e5c003388e024f78c4f4e9
Jan Michael Alonzo <jmalonzo@webkit.org>
2010-01-31 17:52:51 +1100

    Support leiningen for build management

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-oauth "1.0.0-SNAPSHOT"
+  :description "OAuth Consumer support for Clojure"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                 [com.twinql.clojure/clj-apache-http "1.1.0"]]
+  :dev-dependencies [[org.clojure/swank-clojure "1.1.0"]]
+  )
