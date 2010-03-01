3f08e0ed02736b67bb38fa7a02a7e51f7136d393
Pelle Braendgaard <pelleb@gmail.com>
2010-03-01 18:41:23 -0500

    Added leiningen project file

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clauth "1.0.0-SNAPSHOT" 
+  :description "OAuth Consumer support for Clojure" 
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                [org.clojure/clojure-contrib "1.1.0"]
+                [com.twinql.clojure/clj-apache-http "1.1.0"]]
+)
