863cc69294c78abef0db40e9e0fdec4444790cb8
Pelle Braendgaard <pelleb@gmail.com>
2010-03-02 13:22:56 -0500

    Further a long in implementation of server side. Signature does not match. So the most important test currently fails. I suspect it is an encoding issue and will do deeper tests next on those.

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "OAuth Consumer support for Clojure" 
   :dependencies [[org.clojure/clojure "1.1.0"]
                 [org.clojure/clojure-contrib "1.1.0"]
-                [com.twinql.clojure/clj-apache-http "1.1.0"]]
+                [com.twinql.clojure/clj-apache-http "1.1.0"]
+                ]
 )
