5ae197eb18b4867987bf4d3daf90bd5ac55bd89e
Pelle Braendgaard <pelleb@gmail.com>
2010-03-06 20:03:30 -0500

    basic crud token store

--- src/oauth/token_store.clj
+++ src/oauth/token_store.clj
@@ -4,13 +4,51 @@
   oauth.token-store
 )
 
+;; Return a Consumer record based on consumer key.
+;; If a consumer is returned then it is assumed to be valid
 (defmulti consumer-get 
   "Get consumer from store"
   (fn [c _] c))
 
+;; Stores a consumer. The 2 required fields in the map are :key and :secret
 (defmulti consumer-put 
   "Stores consumer"
   (fn [c _] c))
+
+;; Return a Request Token record based on a token
+;; If a token is returned then it is assumed to be valid. Thus expiry etc should be handled in the store.
+(defmulti request-token-get 
+  "Get valid request-token from store"
+  (fn [c _] c))
+
+;; Stores a Request Token. The 3 required fields in the map are :token, :secret and :callback_url
+(defmulti request-token-put 
+  "Stores request-token"
+  (fn [c _] c))
+
+;; Revokes a request token. Implementations can chose to simply delete record. 
+;; However once this is called it should no longer be returned by request-token-get
+(defmulti request-token-revoke 
+  "Revokes a request-token"
+  (fn [c _] c))
+
+;; Return a Request Token record based on a token
+;; If a token is returned then it is assumed to be valid. Thus expiry etc should be handled in the store.
+(defmulti access-token-get 
+  "Get valid access-token from store"
+  (fn [c _] c))
+
+;; Stores an Access Token. The 2 required fields in the map are :token and :secret
+(defmulti access-token-put 
+  "Stores access-token"
+  (fn [c _] c))
+
+;; Revokes an access token. Implementations can chose to simply delete record. 
+;; However once this is called it should no longer be returned by access-token-get
+(defmulti access-token-revoke 
+  "Revokes a access-token"
+  (fn [c _] c))
+
 
 (def memory-consumers (atom {}))
 
@@ -22,4 +60,39 @@
 (defmethod consumer-put :memory
   [_ consumer]
     ((swap! memory-consumers assoc (consumer :key) consumer) (consumer :key))
+  )
+
+
+(def memory-request-tokens (atom {}))
+
+(defmethod request-token-get :memory
+  [_ token]
+    (@memory-request-tokens token)
+  )
+
+(defmethod request-token-put :memory
+  [_ token]
+    ((swap! memory-request-tokens assoc (token :token) token) (token :token))
+  )
+
+(defmethod request-token-revoke :memory
+  [_ token]
+    (swap! memory-request-tokens dissoc token )
+  )
+
+(def memory-access-tokens (atom {}))
+
+(defmethod access-token-get :memory
+  [_ token]
+    (@memory-access-tokens token)
+  )
+
+(defmethod access-token-put :memory
+  [_ token]
+    ((swap! memory-access-tokens assoc (token :token) token) (token :token))
+  )
+
+(defmethod access-token-revoke :memory
+  [_ token]
+    (swap! memory-access-tokens dissoc token )
   )
--- test/oauth/token_store_test.clj
+++ test/oauth/token_store_test.clj
@@ -17,3 +17,57 @@
       (is (= (store/consumer-get :memory "consumer") {:key "consumer" :secret "ssh"}))
       )
   )
+  
+  (deftest  
+    #^{:doc "get request-token from memory"}
+    request-token-get
+    (is (= (store/request-token-get :memory nil) nil))
+    (is (= (store/request-token-get :memory "") nil))
+  )
+
+  (deftest  
+    #^{:doc "store request-token in memory"}
+    request-token-put
+    (let [request-token (store/request-token-put :memory {:token "request-token" :secret "ssh"}) ]
+      (is (= request-token {:token "request-token" :secret "ssh"}))
+      (is (= (store/request-token-get :memory "request-token") {:token "request-token" :secret "ssh"}))
+      )
+  )
+
+  (deftest  
+    #^{:doc "revoke request-token in memory"}
+    request-token-revoke
+    (let [request-token (store/request-token-put :memory {:token "request-token" :secret "ssh"}) ]
+      (do
+        (store/request-token-revoke :memory "request-token")
+        (is (= (store/request-token-get :memory "request-token") nil))
+        )
+      )
+  )
+  
+  (deftest  
+    #^{:doc "get access-token from memory"}
+    access-token-get
+    (is (= (store/access-token-get :memory nil) nil))
+    (is (= (store/access-token-get :memory "") nil))
+  )
+
+  (deftest  
+    #^{:doc "store access-token in memory"}
+    access-token-put
+    (let [access-token (store/access-token-put :memory {:token "access-token" :secret "ssh"}) ]
+      (is (= access-token {:token "access-token" :secret "ssh"}))
+      (is (= (store/access-token-get :memory "access-token") {:token "access-token" :secret "ssh"}))
+      )
+  )
+  
+  (deftest  
+    #^{:doc "revoke access-token in memory"}
+    access-token-revoke
+    (let [access-token (store/access-token-put :memory {:token "access-token" :secret "ssh"}) ]
+      (do
+        (store/access-token-revoke :memory "access-token")
+        (is (= (store/access-token-get :memory "access-token") nil))
+        )
+      )
+  )
