2c2e270f5afb0ed9c318a80a6e3bde4e9db9e6d6
Matt Revelle <matt@dynamictyping.org>
2010-03-10 20:00:33 -0500

    Increased minor version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-oauth "1.0.0-SNAPSHOT"
+(defproject clj-oauth "1.1.0-SNAPSHOT"
   :description "OAuth Consumer support for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
