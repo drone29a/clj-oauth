61dc180c3064869d7aec7f803d02f79b805f87a7
Matt Revelle <matt@dynamictyping.org>
2010-03-10 19:58:00 -0500

    Use latest clj-apache-http.

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :description "OAuth Consumer support for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [com.twinql.clojure/clj-apache-http "1.1.0"]]
-  :dev-dependencies [[org.clojure/swank-clojure "1.1.0"]]
-  )
+                 [com.twinql.clojure/clj-apache-http "2.0.0"]]
+  :dev-dependencies [[swank-clojure "1.1.0"]])
