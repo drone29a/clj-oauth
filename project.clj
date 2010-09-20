7f0bfe1ccdf7c2e73e7a621317d9286df390167e
Matt Revelle <matt@dynamictyping.org>
2010-09-20 14:44:53 -0400

    Updated project deps.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-oauth "1.2.3"
   :description "OAuth support for Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [com.twinql.clojure/clj-apache-http "2.0.2"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [com.twinql.clojure/clj-apache-http "2.3.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
 
