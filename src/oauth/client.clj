52c8b1103c0f775d5f4df873242c8ba27ac45ed5
Matt Revelle <matt@dynamictyping.org>
2010-09-20 00:16:56 -0400

    Minor cleanup while bug-hunting.

--- src/oauth/client.clj
+++ src/oauth/client.clj
@@ -7,7 +7,8 @@
             [com.twinql.clojure.http :as http])
   (:use [clojure.contrib.string :only [as-str join split upper-case]]))
 
-(declare success-content)
+(declare success-content
+         authorization-header)
 
 (defstruct #^{:doc "OAuth consumer"} consumer
            :key
@@ -49,17 +50,33 @@
 
 (defn request-token
   "Fetch request token for the consumer."
-  [consumer]
-  (let [unsigned-params (sig/oauth-params consumer)
-        signature (sig/sign consumer (sig/base-string "POST" 
-                                                      (:request-uri consumer)
-                                                      unsigned-params))
-        params (assoc unsigned-params :oauth_signature signature)]
-    (success-content
-     (http/post (:request-uri consumer)
-                :query params
-                :parameters (http/map->params {:use-expect-continue false})
-                :as :urldecoded))))
+  ([consumer]
+     (let [unsigned-params (sig/oauth-params consumer)
+           signature (sig/url-encode (sig/sign consumer
+                                               (sig/base-string "POST" 
+                                                                (:request-uri consumer)
+                                                                unsigned-params)))
+           params (assoc unsigned-params :oauth_signature signature)]
+       (success-content
+        (http/post (:request-uri consumer)
+                   :headers {"Authorization" (authorization-header (assoc params
+                                                                     :oauth_signature signature))}
+                   :parameters (http/map->params {:use-expect-continue false})
+                   :as :urldecoded))))
+  ([consumer callback-uri]
+     (let [unsigned-params (assoc (sig/oauth-params consumer)
+                             :oauth_callback (sig/url-encode callback-uri))
+           signature (sig/url-encode (sig/sign consumer
+                                               (sig/base-string "POST" 
+                                                                (:request-uri consumer)
+                                                                unsigned-params)))
+           params (assoc unsigned-params :oauth_signature signature)]
+       (success-content
+        (http/post (:request-uri consumer)
+                   :headers {"Authorization" (authorization-header (assoc params
+                                                                     :oauth_signature signature))}
+                   :parameters (http/map->params {:use-expect-continue false})
+                   :as :urldecoded)))))
 
 (defn user-approval-uri
   "Builds the URI to the Service Provider where the User will be prompted
@@ -85,15 +102,20 @@ to approve the Consumer's access to their account."
   ([consumer request-token]
      (access-token consumer request-token nil))
   ([consumer request-token verifier]
-     (let [unsigned-params (sig/oauth-params consumer (:oauth_token request-token) verifier)
-           signature (sig/sign consumer (sig/base-string "POST"
-                                                         (:access-uri consumer)
-                                                         unsigned-params)
+     (let [unsigned-params (sig/oauth-params consumer
+                                             (:oauth_token request-token)
+                                             verifier)
+           signature (sig/sign consumer
+                               (sig/base-string "POST"
+                                                (:access-uri consumer)
+                                                unsigned-params)
                                (:oauth_token_secret request-token))
-           params (assoc unsigned-params :oauth_signature signature)]
+           params (assoc unsigned-params
+                    :oauth_signature signature)]
        (success-content
         (http/post (:access-uri consumer)
                    :query params
+;                   :header {"Authorization" (authorization-header params)}
                    :parameters (http/map->params {:use-expect-continue false})
                    :as :urldecoded)))))
 
@@ -101,32 +123,36 @@ to approve the Consumer's access to their account."
   "Return authorization credentials needed for access to protected resources.  
 The key-value pairs returned as a map will need to be added to the 
 Authorization HTTP header or added as query parameters to the request."
-  [consumer token token-secret request-method request-uri & [request-params]]
-  (let [unsigned-oauth-params (sig/oauth-params consumer token)
-        unsigned-params (merge request-params 
-                               unsigned-oauth-params)
-        signature (sig/sign consumer 
-                            (sig/base-string (-> request-method
-                                                 as-str
-                                                 upper-case)
-                                             request-uri
-                                             unsigned-params)
-                            token-secret)]
-    (assoc unsigned-oauth-params :oauth_signature signature)))
+  ([consumer token token-secret request-method request-uri & [request-params]]
+     (let [unsigned-oauth-params (sig/oauth-params consumer token)
+           unsigned-params (merge request-params 
+                                  unsigned-oauth-params)
+           signature (sig/sign consumer 
+                               (sig/base-string (-> request-method
+                                                    as-str
+                                                    upper-case)
+                                                request-uri
+                                                 unsigned-params)
+                               token-secret)]
+       (assoc unsigned-oauth-params :oauth_signature signature))))
 
 (defn authorization-header
   "OAuth credentials formatted for the Authorization HTTP header."
-  [realm credentials]
-  (str "OAuth " (join "," (map (fn [[k v]] 
-                                 (str (as-str k) "=\"" v "\""))
-                               (assoc credentials :realm realm)))))
+  ([oauth-params]
+     (str "OAuth " (join ", " (map (fn [[k v]] 
+                                    (str (as-str k) "=\"" v "\""))
+                                  oauth-params))))
+  ([oauth-params realm]
+     (authorization-header (assoc oauth-params realm))))
 
 (defn check-success-response [m]
   (let [code (:code m)
         reason (:reason m)]
     (if (or (< code 200)
             (>= code 300))
-      (throw (new Exception (str "Got non-success code: " code ". Reason: " reason)))
+      (throw (new Exception (str "Got non-success code: " code ". "
+                                 "Reason: " reason ", "
+                                 "Content: " (:content m))))
       m)))
 
 (defn success-content [m]
