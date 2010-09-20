24998f3781cb6d54e2a3dc4b71e263313ffb5481
Matt Revelle <matt@dynamictyping.org>
2010-09-20 00:19:25 -0400

    New tests using actual services.

---
+++ test/oauth/client_twitter_test.clj
@@ -0,0 +1,17 @@
+(ns oauth.client-twitter-test
+  (:require [oauth.client :as oc])
+  (:use clojure.test)
+  (:load "twitter_keys"))
+
+(def consumer (oc/make-consumer key
+                                secret
+                                "https://api.twitter.com/oauth/request_token"
+                                "https://api.twitter.com/oauth/access_token"
+                                "https://api.twitter.com/oauth/authorize"
+                                :hmac-sha1))
+
+(deftest
+    #^{:doc "Test requesting a token from Twitter.
+            Considered to pass if no exception is thrown."}
+  request-token
+  (oc/request-token consumer))
