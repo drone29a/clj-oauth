679784a204f00a50706cf9b2af52d595dd2d3dcd
Matt Revelle <matt@dynamictyping.org>
2010-11-09 03:30:04 -0500

    Bumped version number.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-oauth "1.2.3"
+(defproject clj-oauth "1.2.5"
   :description "OAuth support for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.twinql.clojure/clj-apache-http "2.3.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
 
