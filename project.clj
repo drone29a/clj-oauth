07aedea2aafec9df711e96d6bc243827bcf52b7a
Matt Revelle <mrevelle@gmail.com>
2011-02-14 19:48:49 -0800

    Force use of HttpClient 4.1 rather than 4.0 as specified by clj-apache-http dep.

--- project.clj
+++ project.clj
@@ -2,6 +2,9 @@
   :description "OAuth support for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [com.twinql.clojure/clj-apache-http "2.3.1"]]
+                 [com.twinql.clojure/clj-apache-http "2.3.1"]
+                 [org.apache.httpcomponents/httpclient "4.1"]
+                 [org.apache.httpcomponents/httpcore "4.1"]
+                 [org.apache.httpcomponents/httpmime "4.1"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
 
