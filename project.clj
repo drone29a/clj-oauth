7d88e3876263e658ff593246c771e18f7b00b72a
Roman Gonzalez <romanandreg@gmail.com>
2012-01-07 20:07:22 -0800

    Upgrading library to support clojure v1.3

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject clj-oauth "1.2.10-SNAPSHOT"
   :description "OAuth support for Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.twinql.clojure/clj-apache-http "2.3.1"]
                  [org.apache.httpcomponents/httpclient "4.1"]
                  [org.apache.httpcomponents/httpcore "4.1"]
