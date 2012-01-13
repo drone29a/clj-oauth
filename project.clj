2c36d2c8206dfcb3a131a4264e09456e3d866ac6
Tavis Rudd <tavis@damnsimple.com>
2012-01-13 10:01:32 -0800

    add :repositories for access to forked version of clj-apache-http

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clj-oauth "1.3.0-SNAPSHOT"
   :description "OAuth support for Clojure"
+  :repositories {"snapshots" {:url "s3p://lein-snapshots/snapshots"}}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.twinql.clojure/clj-apache-http "2.3.1"]
+                 [com.twinql.clojure/clj-apache-http "2.3.2-SNAPSHOT"]
                  [org.apache.httpcomponents/httpclient "4.1"]
                  [org.apache.httpcomponents/httpcore "4.1"]
                  [org.apache.httpcomponents/httpmime "4.1"]])
