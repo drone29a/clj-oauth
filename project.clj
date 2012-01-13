c638c95eb1e7b53cebda420ce6cc71ae914b98f6
Tavis Rudd <tavis@damnsimple.com>
2012-01-13 09:36:03 -0800

    bump version number

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
-(defproject clj-oauth "1.2.10-SNAPSHOT"
+(defproject clj-oauth "1.3.0-SNAPSHOT"
   :description "OAuth support for Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.twinql.clojure/clj-apache-http "2.3.1"]
                  [org.apache.httpcomponents/httpclient "4.1"]
                  [org.apache.httpcomponents/httpcore "4.1"]
                  [org.apache.httpcomponents/httpmime "4.1"]])
-
-
