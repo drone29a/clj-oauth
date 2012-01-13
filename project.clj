5219cbc9853f8e479c33a5d7b4ae012bfcbc9e28
Tavis Rudd <tavis@damnsimple.com>
2012-01-13 11:31:26 -0800

    rm transitive deps org.apache.httpcomponents that aren't used directly here

--- project.clj
+++ project.clj
@@ -3,6 +3,4 @@
   :repositories {"snapshots" {:url "s3p://lein-snapshots/snapshots"}}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.twinql.clojure/clj-apache-http "2.3.2-SNAPSHOT"]
-                 [org.apache.httpcomponents/httpclient "4.1"]
-                 [org.apache.httpcomponents/httpcore "4.1"]
-                 [org.apache.httpcomponents/httpmime "4.1"]])
+                 ])
