022b0e9088a2e4976b798fa1307be27161756b76
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2014-03-24 23:12:14 +0200

    Bouncy Castle Provider back in dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-oauth "1.5.0"
   :description "OAuth support for Clojure"
   :dependencies [[org.clojure/clojure "1.5.0"]
+                 [bouncycastle/bcprov-jdk16 "140"]
                  [commons-codec/commons-codec "1.8"]
                  [clj-http "0.9.1"]]
   :min-lein-version "2.0.0")
