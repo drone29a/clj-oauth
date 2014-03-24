abef5f7d6d865716b274d4b7e443c8597d9a8f08
Matt Revelle <mrevelle@gmail.com>
2014-03-24 07:42:02 -0400

    Added back and updated bouncycastle dep.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-oauth "1.5.0"
+(defproject clj-oauth "1.5.1"
   :description "OAuth support for Clojure"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [commons-codec/commons-codec "1.8"]
-                 [clj-http "0.9.1"]]
-  :min-lein-version "2.0.0")
+                 [org.bouncycastle/bcprov-jdk16 "1.46"]
+                 [clj-http "0.9.1"]])
