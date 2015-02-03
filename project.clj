03ebc9c2442517e41fd1822796cb8b11e67d9b5d
Matt Revelle <mrevelle@gmail.com>
2015-02-02 21:04:31 -0500

    Updated project version and dependencies.

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
-(defproject clj-oauth "1.5.2-SNAPSHOT"
+(defproject clj-oauth "1.5.3-SNAPSHOT"
+  :url "https://github.com/mattrepl/clj-oauth"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :description "OAuth support for Clojure"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [commons-codec/commons-codec "1.8"]
                  [org.bouncycastle/bcprov-jdk15on "1.50"]
                  [org.bouncycastle/bcpkix-jdk15on "1.50"]
-                 [clj-http "0.9.1"]])
+                 [clj-http "1.0.1"]])
