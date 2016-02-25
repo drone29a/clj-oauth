94af448197167535beb4d531f72d1a0238ac80a2
Matt Revelle <mrevelle@gmail.com>
2016-02-24 23:35:39 -0500

    Update to next version snapshot for future development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-oauth "1.5.5"
+(defproject clj-oauth "1.5.6-SNAPSHOT"
   :url "https://github.com/mattrepl/clj-oauth"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
