64a369231be1b6994aec0ee7f5079e3c62f38b99
Raymond Huang <rymndhng@gmail.com>
2018-01-04 20:03:40 -0800

    Bump bouncycastle dependencies

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :description "OAuth support for Clojure"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [commons-codec/commons-codec "1.8"]
-                 [org.bouncycastle/bcprov-jdk15on "1.54"]
-                 [org.bouncycastle/bcpkix-jdk15on "1.54"]
                  [clj-http "2.0.1"]])
+                 [org.bouncycastle/bcprov-jdk15on "1.59"]
+                 [org.bouncycastle/bcpkix-jdk15on "1.59"]
